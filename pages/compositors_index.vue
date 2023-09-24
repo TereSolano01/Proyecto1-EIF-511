aa2a9e163d346e13cfab2aca65dc68bba180343f
TereSolano01 <tere.solano.solis@est.una.ac.cr>
2023-09-23 19:47:11 -0600

    test

--- pages/compositors_index.vue
+++ pages/compositors_index.vue
@@ -1,26 +1,26 @@
-<!-- pages/compositors_index.vue -->
+<script>
+  export default {
+    async asyncData({ $content, params }) {
+      const authors = await $content('compositors').fetch()
+      return {
+        compositors
+      }
+    }
+  }
+</script>
+
 <template>
-  <div>
-    <HeaderView />
-    <h2 style="margin-top: 15px">Compositores</h2>
-    <p>Esta sección presenta información sobre compositores.</p>
-    <div class="container centered-content">
-      <ul>
-        <ContentList path="/compositors" v-slot="{ list }">
-          <div class="grid-container">
-            <span v-for="compositor in list" :key="compositor._path" class="grid-item">
-              <NuxtLink :to="compositor._path">
-                <img class="u-max-full-width" :src="'/images/'+compositor.image" alt="Descripción de la imagen">
-                <NuxtLink :to="compositor._path">{{ compositor.name }}</NuxtLink>
-                <p class="goto"><NuxtLink :to="compositor._path">Ver compositor<i class="fa-solid fa-chevron-right" style="color: #000000;"></i></NuxtLink></p>
-              </NuxtLink>
-            </span>
-          </div>
-        </ContentList>
-      </ul>
-    </div>
-    <FooterView />
-  </div>
+  <div class="container">
+   <HeaderView />
+   <h3 style="margin-top: 15px">Authors Information</h3>
+     <p>This section presents information about authors</p>
+   <ul>
+     <li v-for="author of authors" :key="author.slug">
+       <NuxtLink :to="{ name: 'compositors-slug', params: { slug: author.slug } }">{{author.name}}</NuxtLink>
+     </li>
+   </ul>
+   <FooterView />
+ </div>
 </template>
 <style scoped>
 @import url('../static/css/indexCompositores.css');
