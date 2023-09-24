58b77d56b24eae24d7ced29ec99849a7732e17c2
TereSolano01 <tere.solano.solis@est.una.ac.cr>
2023-09-23 23:33:38 -0600

    a

--- pages/albums_index.vue
+++ pages/albums_index.vue
@@ -10,8 +10,8 @@
           <div class="grid-container">
             <span v-for="album in list" :key="album._path" class="grid-item">
   <NuxtLink :to="album._path">
-    <p> Nacionalidad: {{album.image}}</p>
-    <img class="u-max-full-width" :src="album.image ? '/static/images/' + album.image : '/static/images/movies/Black-Panther-Wakanda-Forever-Soundtrack.jpg'" alt="">
+    <img src="/static/images/albums/Aladdin2019soundtrac.jpeg" alt="Imagen de la noticia" />
+    <img class="u-max-full-width" :src="'/static/images/'+album.image" alt="">
     <NuxtLink :to="album._path">{{ album.title }}</NuxtLink>
     <p class="goto"><NuxtLink :to="album._path">Ver album<i class="fa-solid fa-chevron-right" style="color: #000000;"></i></NuxtLink></p>
   </NuxtLink>
