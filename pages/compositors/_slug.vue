d6717276f75c839e3549b36077d6db9f3ca67ac2
TereSolano01 <tere.solano.solis@est.una.ac.cr>
2023-09-23 19:53:22 -0600

    test

---
+++ pages/compositors/_slug.vue
@@ -0,0 +1,39 @@
+<script>
+  export default {
+    async asyncData({ $content, params }) {
+      const compositor = await $content('compositors', params.slug).fetch()
+      const books = await $content('albums').where({ compositorId: params.slug }).fetch()
+      return { compositor, books }
+    }
+  }
+</script>
+
+<template>
+  <div class="container">
+   <HeaderView />
+   <div class="row">
+     <div class="three columns">
+       <img class="u-max-full-width" :src="'/images/'+compositor.image" alt="Italian Trulli">
+     </div>
+     <div class="six columns">
+       <h4>{{compositor.name}}</h4>
+	   Nationality: {{compositor.nationality}}; Born: {{compositor.birth_year}}</br>
+	   Fields: {{compositor.fields}}</br></br>
+	   <b>Biography</b></br>
+	    <nuxt-content :document="compositor" />
+	 </div>
+	 <div class="three columns"></div>
+	   <h5>Books</h5>
+	   <ul>
+	     <li v-for="book of books" :key="book.slug">
+	       <NuxtLink :to="{ name: 'compositor-slug', params: { slug: book.slug } }">{{book.title}}</NuxtLink>
+	     </li>
+	   </ul>
+   </div>
+   <FooterView />
+ </div>
+</template>
+
+<style scoped>
+@import url('../../static/css/details.css');
+</style>
