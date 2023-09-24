961bc311e9e70b318a195d932470d47adb9719af
TereSolano01 <tere.solano.solis@est.una.ac.cr>
2023-09-23 22:33:12 -0600

    test

--- pages/albums_index.vue
+++ pages/albums_index.vue
@@ -10,7 +10,7 @@
           <div class="grid-container">
             <span v-for="album in list" :key="album._path" class="grid-item">
   <NuxtLink :to="album._path">
-    <img class="u-max-full-width" :src="'static/images/'+album.image" alt="Descripción de la imagen">
+    <img class="u-max-full-width" :src="'static/images/'+album.image" alt="">
     <NuxtLink :to="album._path">{{ album.title }}</NuxtLink>
     <p class="goto"><NuxtLink :to="album._path">Ver album<i class="fa-solid fa-chevron-right" style="color: #000000;"></i></NuxtLink></p>
   </NuxtLink>
