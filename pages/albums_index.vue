06793fb47f03f1170e484dba2290846bd312e453
TereSolano01 <tere.solano.solis@est.una.ac.cr>
2023-09-23 23:42:55 -0600

    as

--- pages/albums_index.vue
+++ pages/albums_index.vue
@@ -10,8 +10,7 @@
           <div class="grid-container">
             <span v-for="album in list" :key="album._path" class="grid-item">
   <NuxtLink :to="album._path">
-    <img src="/static/images/albums/Aladdin2019soundtrac.jpeg" alt="Imagen de la noticia" />
-    <img class="u-max-full-width" :src="'/static/images/'+album.image" alt="">
+    <img class="u-max-full-width" :src="'/images/'+album.image" alt="Italian Trulli">
     <NuxtLink :to="album._path">{{ album.title }}</NuxtLink>
     <p class="goto"><NuxtLink :to="album._path">Ver album<i class="fa-solid fa-chevron-right" style="color: #000000;"></i></NuxtLink></p>
   </NuxtLink>
