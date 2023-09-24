a16ef9a3803d6f4fcf62cf7d5fdd8b18aeac56be
TereSolano01 <tere.solano.solis@est.una.ac.cr>
2023-09-23 21:18:49 -0600

    test

--- pages/compositors_index.vue
+++ pages/compositors_index.vue
@@ -1,9 +1,9 @@
 <script>
   export default {
     async asyncData({ $content, params }) {
-      const compositors = await $content('compositors').fetch()
+      const authors = await $content('compositors').fetch()
       return {
-        compositors
+        authors
       }
     }
   }
@@ -15,14 +15,13 @@
    <h3 style="margin-top: 15px">Authors Information</h3>
      <p>This section presents information about authors</p>
    <ul>
-     <li v-for="compositor of compositors" :key="compositor.slug">
-       <NuxtLink :to="{ name: 'compositors-slug', params: { slug: compositor.slug } }">{{compositor.name}}</NuxtLink>
+     <li v-for="author of authors" :key="author.slug">
+       <NuxtLink :to="{ name: 'authors-slug', params: { slug: author.slug } }">{{author.name}}</NuxtLink>
      </li>
    </ul>
    <FooterView />
  </div>
 </template>
-
 <style scoped>
 @import url('../static/css/indexCompositores.css');
 
